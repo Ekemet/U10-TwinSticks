1eb5ef10691232d946a90bda4b51a3b70d08caed
DESKTOP-FAD3K4A\MIRAGE <shmazirek@gmail.com>
2017-07-29 21:59:19 -0500

    U10-TwinSticks 01 -> Base version

---
+++ Assets/Player.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    private void Update() {
+        Debug.Log("H: " + CrossPlatformInputManager.GetAxis("Horizontal"));
+        Debug.Log("V: " + CrossPlatformInputManager.GetAxis("Vertical"));
+    }
+
+}
